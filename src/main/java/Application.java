b62a55eec9e251e3d564412a41686192374f06a3
RoxasLxy <76763927+RoxasLxy@users.noreply.github.com>
2021-02-19 22:24:36 -0800

    commit here

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -4,6 +4,7 @@ import com.leapcode.service.SpeakerServiceImpl;
 public class Application {
     public static void main(String[] args){
         // this is eddited by xiangyu li 20200219
+        //142345664644464
         SpeakerService service = new SpeakerServiceImpl();
         System.out.println(service.findAll().get(0).getFirstName());
         System.out.println(service.findAll().get(0).getLastName());
